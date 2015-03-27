ef76a41820ca987b2f71114ab93237152d69b3ff
Tyler <tyounkin@ycp.edu>
2015-03-27 09:35:47 -0400

    Test

---
+++ CourseSurveyProjectWebApp/src/edu/ycp/cs320/coursesurvey/servlets/SurveyCreation.java
@@ -0,0 +1,18 @@
+package edu.ycp.cs320.coursesurvey.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SurveyCreation extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// Just forward to the view
+		req.getRequestDispatcher("/_view/surveycreation.jsp").forward(req, resp);
+	}
+}

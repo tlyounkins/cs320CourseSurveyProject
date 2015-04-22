d33efa4bf08304d72acbf3ce361b1083bf82a9dd
cdawson717 <cdawson3@ycp.edu>
2015-04-22 08:44:07 -0400

    Test

---
+++ CourseSurveyProjectWebApp/src/edu/ycp/cs320/coursesurvey/controller/AccountCreationController.java
@@ -0,0 +1,58 @@
+package edu.ycp.cs320.coursesurvey.controller;
+
+import edu.ycp.cs320.coursesurvey.model.Institution;
+import edu.ycp.cs320.coursesurvey.persistence.DatabaseProvider;
+import edu.ycp.cs320.coursesurvey.persistence.FakeDatabase;
+
+public class AccountCreationController {
+	private boolean passwordMatch = false;
+	private boolean finished = false;
+	private boolean instExists = false;
+
+	
+	private void checkPassword(String password, String passwordCheck){
+		if (password.contains(passwordCheck)){
+			passwordMatch = true;
+			System.out.println("match");
+		}
+		else {
+			passwordMatch = false;
+			System.out.println(password + " mismatch " + passwordCheck);
+		}
+	}
+
+	public boolean passwordsMatching(){
+		return this.passwordMatch;
+	}
+	
+	public boolean instAlreadyExists(){
+		return this.instExists;
+	}
+
+	public void createAccount(String instName, String accountName, String password, String passwordCheck){
+		this.checkPassword(password, passwordCheck);
+		if (passwordMatch) {
+
+			int id = -1;
+			
+			//create a new institution if it does not exist
+			if ((DatabaseProvider.getInstance().findInstitution(instName)) == null){
+				id = DatabaseProvider.getInstance().addInstitution(instName);	
+				DatabaseProvider.getInstance().addAdmin(accountName, password, id);
+			}
+			else {
+				//variable will be set to true if the institution already exists and the account will not be created
+				instExists = true;
+			}
+			
+			//this.institute.createAdminAccount(accountName, password);
+			instExists = false;
+			finished = true;
+			System.out.println("created");
+		}
+	}
+
+	public boolean done(){
+		return this.finished;
+	}
+}

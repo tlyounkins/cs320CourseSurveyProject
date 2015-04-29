3aa8cdac5e88490ddc5c878d7c5070314c919e46
Garlan <gabowser@gmail.com>
2015-04-28 23:28:11 -0400

    Added missing functions for Survey submissios to fake db

--- CourseSurveyProject/src/edu/ycp/cs320/coursesurvey/model/ResponseIndex.java
+++ CourseSurveyProject/src/edu/ycp/cs320/coursesurvey/model/ResponseIndex.java
@@ -1,16 +1,16 @@
 package edu.ycp.cs320.coursesurvey.model;
 
 public class ResponseIndex {
-	private int responseNum;
+	//private int responseNum;
 	private int responseID;
 	
-	public void setResponseNum(int responseNum){
+	/*public void setResponseNum(int responseNum){
 		this.responseNum = responseNum;
 	}
 	
 	public int getResponseNum(){
 		return this.responseNum;
-	}
+	}*/
 	
 	public void setResponseID(int responseID){
 		this.responseID = responseID;
